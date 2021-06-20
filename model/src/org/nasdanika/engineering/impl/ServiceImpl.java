885c536ca9a32a6bac4fd892b8933ddc2b2e0840
Pavel <Pavel.Vlasov@nasdanika.org>
2021-06-19 21:08:29 -0400

    Service

---
+++ model/src/org/nasdanika/engineering/impl/ServiceImpl.java
@@ -0,0 +1,176 @@
+/**
+ */
+package org.nasdanika.engineering.impl;
+
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.nasdanika.engineering.Activity;
+import org.nasdanika.engineering.EngineeringPackage;
+import org.nasdanika.engineering.Service;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Service</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * </p>
+ * <ul>
+ *   <li>{@link org.nasdanika.engineering.impl.ServiceImpl#getTarget <em>Target</em>}</li>
+ * </ul>
+ *
+ * @generated
+ */
+public class ServiceImpl extends JourneyElementImpl implements Service {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ServiceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return EngineeringPackage.Literals.SERVICE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Activity getTarget() {
+		return (Activity)eDynamicGet(EngineeringPackage.SERVICE__TARGET, EngineeringPackage.Literals.SERVICE__TARGET, true, true);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Activity basicGetTarget() {
+		return (Activity)eDynamicGet(EngineeringPackage.SERVICE__TARGET, EngineeringPackage.Literals.SERVICE__TARGET, false, true);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetTarget(Activity newTarget, NotificationChain msgs) {
+		msgs = eDynamicInverseAdd((InternalEObject)newTarget, EngineeringPackage.SERVICE__TARGET, msgs);
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void setTarget(Activity newTarget) {
+		eDynamicSet(EngineeringPackage.SERVICE__TARGET, EngineeringPackage.Literals.SERVICE__TARGET, newTarget);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseAdd(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case EngineeringPackage.SERVICE__TARGET:
+				Activity target = basicGetTarget();
+				if (target != null)
+					msgs = ((InternalEObject)target).eInverseRemove(this, EngineeringPackage.ACTIVITY__SERVICES, Activity.class, msgs);
+				return basicSetTarget((Activity)otherEnd, msgs);
+		}
+		return super.eInverseAdd(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case EngineeringPackage.SERVICE__TARGET:
+				return basicSetTarget(null, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case EngineeringPackage.SERVICE__TARGET:
+				if (resolve) return getTarget();
+				return basicGetTarget();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case EngineeringPackage.SERVICE__TARGET:
+				setTarget((Activity)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case EngineeringPackage.SERVICE__TARGET:
+				setTarget((Activity)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case EngineeringPackage.SERVICE__TARGET:
+				return basicGetTarget() != null;
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //ServiceImpl
